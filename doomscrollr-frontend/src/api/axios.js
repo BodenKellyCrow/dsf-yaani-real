f56c2f5c4868cb47fd716c13147aaa0139d694c3
BodenKellyCrow <bossalando177@gmail.com>
2025-07-28 17:07:43 +0000

    api connection

--- doomscrollr-frontend/src/api/axios.js
+++ doomscrollr-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://doomscrollr-backend.onrender.com', // your backend URL here
+  baseURL: 'https://doomscrollr-backend.onrender.com/api', // your backend URL here
   withcredentials: true
 });
 
